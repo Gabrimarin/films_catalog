20d9b23efb4a65eae8fbec506f701c9af8d64bfb
Gabrimarin <gabriel12367@live.com>
2020-10-16 19:52:21 -0300

    modificação nas rotas para a api

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,6 +1,6 @@
 const AUTH_KEY = '1119a5630b568c4834abd1b6735244de';
 const BASE_ROUTE = 'https://api.themoviedb.org/3';
-const getGeneralRoute = (route) => {
+export const getGeneralRoute = (route) => {
   return `${BASE_ROUTE}/${route}?api_key=${AUTH_KEY}`;
 };
 
@@ -12,3 +12,7 @@ export const GET_BY_GENRES = (genres, page) =>
 
 export const GET_POSTER_PATH = (path) =>
   'https://image.tmdb.org/t/p/original' + path;
+
+
+export const GET_MOVIE_PATH = (path) =>
+'https://image.tmdb.org/t/p/original' + path;
