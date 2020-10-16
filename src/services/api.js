c9b01d812242fcd122b8769d47fd0105579f832c
Gabrimarin <gabriel12367@live.com>
2020-10-16 19:47:33 -0300

    Adição de funções para busca na api

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,23 @@
 import Axios from 'axios';
+import {getGeneralRoute} from '../constants';
 
 export default class RestServices {
   async get(route) {
     try {
       const response = await Axios.get(route);
       return response;
+    } catch (error) {
+      return {
+        has_error: true,
+        error: error.status_message,
+      };
+    }
+  }
+
+  async getMovie(id) {
+    try {
+      const response = await Axios.get(getGeneralRoute(`movie/${id}`));
+      return response.data;
     } catch (error) {
       return {
         has_error: true,
